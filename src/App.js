1639b67cee0f22d2269a867aae30f42663a38307
kriti2020 <96840464+kriti2020@users.noreply.github.com>
2022-12-14 23:42:42 +0530

    adding starting files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Shopverse</a>
+      </header>
+      <main>Product List</main>
+    </div>
+  );
+}
+
+export default App;
